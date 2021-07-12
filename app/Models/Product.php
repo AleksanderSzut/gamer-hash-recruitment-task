d48bba306da964a4a7a32f9d6f3dda06548b1cc4
Aleksander <szut.aleksander@gmail.com>
2021-07-12 02:56:57 +0200

    - add relations to model - add cache to product and category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Facades\Cache;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'description'
+    ];
+
+    public function cacheKey(): string
+    {
+        return sprintf(
+            "%s/%s-%s",
+            $this->getTable(),
+            $this->getKey(),
+            $this->updated_at
+        );
+    }
+
+    public function getCachedCategories()
+    {
+        return Cache::remember($this->cacheKey() . ':categories', 15, function () {
+            return $this->categories;
+        });
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+
 }
